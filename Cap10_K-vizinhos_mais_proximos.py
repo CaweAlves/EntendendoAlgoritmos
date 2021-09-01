859a9e116d34b466b89c0ab0364480fefb994b87
Cawe Alves <84283988+Robertao541@users.noreply.github.com>
2021-09-01 15:24:16 -0300

    Ultimo Capitulo do Livro

---
+++ Cap10_K-vizinhos_mais_proximos.py
@@ -0,0 +1,7 @@
+# TEORIA DE PITAGORAS, FORMULA PARA SABER A DISTANCIA ENTRE DUAS OU MAIS DIMENSÕES
+
+ # √(a-a)² + (b-b)² e assim por diante.
+
+ #(4-2)² + (3-5)² + (5-1)² + (1-3)² + (5-1)²
+ #  4    +  -4    +   8    +  -4    +   8
+ # 24 
